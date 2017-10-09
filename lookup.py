260f23d9faa1b4a7128e90a4baeccee4fbd5ae5c
l33tmarmot <github@dogstarsecurity.com>
2017-10-09 06:54:17 -0700

    initial

--- lookup.py
+++ lookup.py
@@ -5,7 +5,7 @@ from pprint import pprint
 
 class NetRecord:
     def __init__(self, network, country, name, events):
-        self.cidr = IPv4Network(network)  # todo: Handle multiple networks returned by RDAP that are comma-separated.
+        self.cidr = IPv4Network(network)
         self.country_code = country
         self.name = name
         self.events = events
@@ -26,7 +26,7 @@ class NetRecord:
 
 def lookup_single_ip(ip_str):
     ip_record = IPWhois(ip_str)
-    results = ip_record.lookup_rdap(depth=1)
+    results = ip_record.lookup_rdap(depth=0)
     return results
 
 
@@ -38,34 +38,34 @@ output_data = {}
 for ip in ip_list:
     ip_obj = ip_address(ip)
     for major_net in cachedata.keys():
-        cached_net = ip_network(major_net)
+        cached_net = ip_network(major_net.strip())
         if ip_obj in cached_net:
             print("Cache Hit: {0} is in network {1}".format(ip, major_net))
             output_data[ip] = cachedata[major_net]
             break  # Cache hit, move to the next IP.
     else:
         info_dict = lookup_single_ip(ip)
-
-        new_net = info_dict['network']['cidr']
+        new_nets = info_dict['network']['cidr'].split(',')
+        # new_net = info_dict['network']['cidr']
         new_net_country = info_dict['network']['country']
         new_net_name = info_dict['network']['name']
         new_net_events = info_dict['network']['events']
         new_net_asn = info_dict['asn_cidr']
+        for new_net in new_nets:
+            new_net_obj = NetRecord(new_net.strip(), new_net_country, new_net_name, new_net_events)
+            new_asn_dict = {'asn': info_dict['asn'],
+                            'asn_country_code': info_dict['asn_country_code'],
+                            'asn_date': info_dict['asn_date'],
+                            'asn_description': info_dict['asn_description'],
+                            'asn_registry': info_dict['asn_registry']}
+            new_net_obj.add_asn(new_net_asn, new_asn_dict)
 
-        new_net_obj = NetRecord(new_net, new_net_country, new_net_name, new_net_events)
-        new_asn_dict = {'asn': info_dict['asn'],
-                        'asn_country_code': info_dict['asn_country_code'],
-                        'asn_date': info_dict['asn_date'],
-                        'asn_description': info_dict['asn_description'],
-                        'asn_registry': info_dict['asn_registry']}
-        new_net_obj.add_asn(new_net_asn, new_asn_dict)
-
-        cachedata[new_net] = new_net_obj
-        output_data[ip] = new_net_obj
-        print("New Record:{0}".format(ip))
+            cachedata[new_net] = new_net_obj
+            output_data[ip] = new_net_obj
+            print("New Record:{0}".format(ip))
 
 
 print("List complete, printing output...")
 for ip, info in output_data.items():
     print("{0} Network >> {1}".format(ip, str(info.cidr)))
-    print("\t{0}".format(info.find_asn_data(ip)))
+    #print("\t{0}".format(info.find_asn_data(ip)))
